251795eab556f2fd6302a0cba26ff4d154e6278f
ky948 <kundanyadav948@gmail.com>
2026-09-19 15:39:57 +0530

    Use versioned S3 Terraform state with locking

--- infra/aws/versions.tf
+++ infra/aws/versions.tf
@@ -1,11 +1,29 @@
 terraform {
-  required_version = ">= 1.8.0"
+  required_version = ">= 1.10.0"
+
   required_providers {
-    aws = { source = "hashicorp/aws", version = "~> 5.0" }
-    random = { source = "hashicorp/random", version = "~> 3.6" }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
   }
+
+  backend "s3" {}
 }
+
 provider "aws" {
   region = var.aws_region
-  default_tags { tags = { Project = var.name, Environment = var.environment, ManagedBy = "Terraform" } }
+
+  default_tags {
+    tags = {
+      Project     = var.name
+      Environment = var.environment
+      ManagedBy   = "Terraform"
+    }
+  }
 }
